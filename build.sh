e1033db83442baba54bf4ba79266fb9405da157b
Joost van de Wijgerd <jwijgerd@gmail.com>
2020-03-29 11:22:40 +0200

    updated to quarkus 1.3.1 that fixes the issue with the imagePullPolicy

--- build.sh
+++ build.sh
@@ -5,17 +5,12 @@ kubectl config set-context --current --namespace=elasticactors
 # create rbac rules and crds
 kubectl apply -f deploy/
 # now package and deploy
-QUARKUS_OPTIONS="-Dquarkus.container-image.build=true -Dquarkus.container-image.push=true -Dquarkus.container-image.group=elasticactors -Dquarkus.container-image.registry=docker.io -Dquarkus.kubernetes.image-pull-policy=Always -Dquarkus.native.native-image-xmx=10G"
+QUARKUS_OPTIONS="-Dquarkus.container-image.build=true -Dquarkus.container-image.push=true -Dquarkus.container-image.group=elasticactors -Dquarkus.container-image.registry=docker.io -Dquarkus.kubernetes.image-pull-policy=Always -Dquarkus.native.native-image-xmx=10G -Dquarkus.kubernetes.deploy=true"
 
 # a profile can be input: build.sh native
-if [ -n "${1}" ]; then
+if [[ -n "${1}" && "native" == "${1}" ]]; then
 ./mvnw -s ../settings.xml -Dmaven.test.skip=true -P${1} package ${QUARKUS_OPTIONS} -Dquarkus.kubernetes.mounts.elasticactors-operator-config.path=/work/config
 else
 ./mvnw -s ../settings.xml -Dmaven.test.skip=true package ${QUARKUS_OPTIONS} -Dquarkus.kubernetes.mounts.elasticactors-operator-config.path=/config
 fi
 
-# this is needed because there is a bug with quarkus.kubernetes.image-pull-policy property no being processed
-# see: https://github.com/quarkusio/quarkus/issues/8091
-#-Dquarkus.kubernetes.deploy=true
-kubectl apply -f target/kubernetes/kubernetes.yml
-kubectl patch deployment elasticactors-operator --patch "$(cat src/main/kubernetes/pull-policy-patch.yaml)"
